c5aa5ca5cacd91449b351eec5f5c1289eafa1a3e
Micol Spitale <micol.spitale@gmail.com>
2019-11-27 12:05:55 -0800

    speaker

---
+++ qt_robot_pi_ws/qt_robot_speaker/scripts/speaker_output.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+from qt_robot_speaker.srv import PlayAudio, PlayAudioResponse
+import pyaudio
+import numpy as np
+import sys
+import time
+import ast
+
+class SpeakerOutput:
+
+    def __init__(self, pi, nuc):
+        self.pi_topic = pi
+	self.nuc_topic = nuc
+        rospy.init_node("speaker_node")
+	speaker_service = rospy.Service('speaker_output', PlayAudio, self.handle_play)
+	self.speak_pub = rospy.Publisher(self.pi_topic+"/speaker_state", String, queue_size=5)        
+	self.output_device_index = None
+        self.get_output_device_index()
+	rospy.spin()
+      
+
+    def handle_play(self, req):
+	print('Received request')
+        data = req.data
+	audio_rate = req.audio_frame
+        p = pyaudio.PyAudio()
+        audio_format = pyaudio.paInt16
+        chunk_size = 512
+        self.stream = p.open(format=audio_format,
+                        channels=1,
+                        rate=audio_rate,
+                        input=False,
+                        output=True,
+                        output_device_index=self.output_device_index,
+                        frames_per_buffer=chunk_size)
+	self.stream.write(data)	
+	time.sleep(1)
+	self.stream.stop_stream()
+	self.stream.close()
+	speaker_state = "Finish speaking"
+	return(speaker_state)
+	
+
+    def get_output_device_index(self):
+        """ Finds the audio device named 'play' configured in ~/.asoundrc.
+            If this device is not found, returns None and pyaudio will use
+            your machines default device
+        """
+        p = pyaudio.PyAudio()
+        rospy.loginfo("Attempting to find device named 'QTSpeaker'")
+        for i in range(p.get_device_count()):
+            device = p.get_device_info_by_index(i)
+	    print(device['name'])
+            if device['name'] == 'play':
+                self.output_device_index = i
+                return
+
+
+if __name__ == '__main__':
+	pi = "qt_robot"
+	nuc = "qtpc"
+    	SpeakerOutput(pi, nuc)
+
---
+++ qt_robot_pi_ws/qt_robot_speaker/scripts/test_speaker.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import rospy
+import sys
+import pyaudio
+import wave
+import struct
+import json
+import time
+import random
+import numpy as np
+from std_msgs.msg import String
+from audio_common_msgs.msg import AudioData
+from qt_robot_speaker.srv import PlayAudio, PlayAudioRequest
+import soundfile as sf
+
+
+WAV_HEADER_LENGTH = 24
+
+class AudioData(object):
+   		 def __init__(self, data):
+		 	self.data = data
+
+class Test():
+	def __init__(self, pi, nuc):
+		self.pi_topic = pi
+		self.nuc_topic = nuc
+		rospy.init_node("speaker_node", anonymous=True)
+		rospy.wait_for_service('speaker_output')
+		self.speaker_output_client = rospy.ServiceProxy('speaker_output', PlayAudio)	
+		self.handle_tts_realtime()	
+		
+		rospy.spin()
+	
+	def handle_speaker_state(self, req):
+		print(req.data)
+		return
+	
+	
+	
+	def handle_tts_realtime(self):
+		outdir = "/home/micolspitale/QTRobot/qt_robot_pi/src/qt_robot_pi/qt_robot_speaker/scripts"
+		phraseID = "1"
+		data, samplerate = sf.read(outdir + '/'+phraseID+'.ogg')
+		sf.write(outdir + '/'+phraseID+'.wav', data, samplerate)
+		file_handle =outdir + '/'+phraseID+'.wav'
+		data = np.fromfile(file_handle, np.uint8)[WAV_HEADER_LENGTH:] #Loading wav file
+		data_array = data.astype(np.uint8).tostring()
+		audio_frame = int(np.int64(samplerate)) 
+		response = self.speaker_output_client(audio_frame, data_array)
+		print(response.speaker_state)
+
+
+	
+		
+
+if __name__ == '__main__':
+	pi =  "qt_robot"
+	nuc = "qtpc"
+    	Test(pi, nuc)
